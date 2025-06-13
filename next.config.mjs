b9aa780c14a15aa095c9795c71021ddae7aa74a7
dipak-trio-spire <dipak@trio-spire.com>
2025-06-13 16:49:14 +0530

    add hostname

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '192.168.1.9',
-        port: '3000',
-        pathname: '/api/media/file/**',
-      },
-    ],
+    domains: ['payload-backend-20uj.onrender.com'], 
   },
   experimental: {
     allowedDevOrigins: ['*'],
